48f9125cac812a7915db8f1442bc235a0a6ffc01
Dawid Łakomy <dawid.lakomy1@gmail.com>
2023-12-24 16:34:57 +0100

    Add jobpage style

--- app/src/main/scala/dlakomy/jobsboard/pages/JobPage.scala
+++ app/src/main/scala/dlakomy/jobsboard/pages/JobPage.scala
@@ -12,6 +12,22 @@ import tyrian.Html.*
 import tyrian.*
 import tyrian.http.*
 
+import scala.scalajs.*
+import scala.scalajs.js.*
+import scala.scalajs.js.annotation.*
+
+
+@js.native
+@JSGlobal()
+class Moment extends js.Object:
+  def fromNow(): String = js.native
+
+
+@js.native
+@JSImport("moment", JSImport.Default)
+object MomentLib extends js.Object:
+  def unix(date: Long): Moment = js.native
+
 
 final case class JobPage(
     id: String,
@@ -40,20 +56,53 @@ final case class JobPage(
   //////////////////////////////////////////
   // ui
   private def renderJobPage(job: Job) =
-    div(`class` := "job-page")(
-      div(`class` := "job-hero")(
-        img(
-          `class` := "job-logo",
-          src     := job.jobInfo.image.getOrElse(""),
-          alt     := job.jobInfo.title
+    div(`class` := "container-fluid the-rock")(
+      div(`class` := "row jvm-jobs-details-top-card")(
+        div(`class` := "col-md-12 p-0")(
+          div(`class` := "jvm-jobs-details-card-profile-img")(
+            img(
+              `class` := "img-fluid",
+              src     := job.jobInfo.image.getOrElse(""),
+              alt     := job.jobInfo.title
+            )
+          ),
+          div(`class` := "jvm-jobs-details-card-profile-title")(
+            h1(s"${job.jobInfo.company} - ${job.jobInfo.title}"),
+            div(`class` := "jvm-jobs-details-card-profile-job-details-company-and-location")(
+              JobComponents.renderJobSummary(job)
+            )
+          ),
+          div(`class` := "jvm-jobs-details-card-apply-now-btn")(
+            a(href := job.jobInfo.externalUrl, target := "blank")(
+              button(`type` := "button", `class` := "btn btn-warning")("Apply now")
+            ),
+            p(
+              MomentLib.unix(job.date / 1000).fromNow()
+            )
+          )
+        )
+      ),
+      div(`class` := "container-fluid")(
+        div(`class` := "container")(
+          div(`class` := "markdown-body overview-section")(
+            renderJobDescription(job)
+          )
         ),
-        h1(s"${job.jobInfo.company} - ${job.jobInfo.title}")
-      ),
-      div(`class` := "job-overview")(
-        JobComponents.renderJobSummary(job)
-      ),
-      renderJobDescription(job),
-      a(href := job.jobInfo.externalUrl, `class` := "job-apply-action", target := "blank")("Apply")
+        div(`class` := "container")(
+          div(`class` := "rok-last")(
+            div(`class` := "row")(
+              div(`class` := "col-md-6 col-sm-6 col-6")(
+                span(`class` := "rock-apply")("Apply for this job.")
+              ),
+              div(`class` := "col-md-6 col-sm-6 col-6")(
+                a(href := job.jobInfo.externalUrl, target := "blank")(
+                  button(`type` := "button", `class` := "rock-apply-btn")("Apply now")
+                )
+              )
+            )
+          )
+        )
+      )
     )
 
   private def renderJobDescription(job: Job) =
@@ -65,11 +114,15 @@ final case class JobPage(
 
     div(`class` := "job-description")().innerHtml(descriptionHtml)
 
-  private def renderNoJobPage() = status.kind.match
-    case StatusKind.LOADING => div("Loading.")
-    case StatusKind.ERROR   => div("The job doesn't exist.")
-    case StatusKind.SUCCESS => div("Huh. This error is unexpected.")
-
+  private def renderNoJobPage() =
+    div(`class` := "container-fluid the-rock")(
+      div(`class` := "row jvm-jobs-details-top-card")(
+        status.kind.match
+          case StatusKind.LOADING => h1("Loading.")
+          case StatusKind.ERROR   => h1("The job doesn't exist.")
+          case StatusKind.SUCCESS => h1("Huh. This error is unexpected.")
+      )
+    )
   // util
   val markdownTransformer = Transformer.from(Markdown).to(HTML).build
 
